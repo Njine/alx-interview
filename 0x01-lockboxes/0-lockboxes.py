2020c646e8a4a4bc9f22f22fbfcd9ed6977bec1a
njine <brikab@gmail.com>
2024-04-19 00:13:04 +0300

    ogla

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-'''Module for managing lockboxes.
+'''A module for working with lockboxes.
 '''
 
 
-def can_unlock_all(boxes):
-    '''Checks if all boxes in a list of boxes can be unlocked given
-    that the first box is unlocked.
+def canUnlockAll(boxes):
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
     '''
     n = len(boxes)
-    seen_boxes = {0}
-    unseen_boxes = set(boxes[0]) - {0}
-    while unseen_boxes:
-        box_idx = unseen_boxes.pop()
-        if not box_idx or not 0 <= box_idx < n:
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
             continue
-        if box_idx not in seen_boxes:
-            unseen_boxes |= set(boxes[box_idx])
-            seen_boxes.add(box_idx)
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
     return n == len(seen_boxes)
-
