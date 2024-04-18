b3c8d427aa705374e2f2ddc6db6bff835480d54b
njine <brikab@gmail.com>
2024-04-19 00:21:40 +0300

    ogola

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,19 +3,17 @@
 '''
 
 
-def can_unlock_all(boxes):
+def canUnlockAll(boxes):
     '''Checks if all boxes in a list of boxes can be unlocked given
     that the first box is unlocked.
     '''
     n = len(boxes)
     seen_boxes = {0}
-    unseen_boxes = set(boxes[0]) - {0}
-    while len(unseen_boxes) > 0:
-        box_idx = unseen_boxes.pop()
-        if not box_idx or not 0 <= box_idx < n:
+    keys_to_check = set(boxes[0]) - {0}
+    while keys_to_check:
+        key = keys_to_check.pop()
+        if not 0 <= key < n:
             continue
-        if box_idx not in seen_boxes:
-            unseen_boxes |= set(boxes[box_idx])
-            seen_boxes.add(box_idx)
-    return n == len(seen_boxes)
-
+        seen_boxes.add(key)
+        keys_to_check |= set(boxes[key]) - seen_boxes
+    return len(seen_boxes) == n
