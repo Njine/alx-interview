8edad3cd3e862547b7538b646540abedc1d5983e
njine <brikab@gmail.com>
2024-04-25 22:16:13 +0300

    round

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,35 +1,40 @@
 #!/usr/bin/python3
-'''Minimum operations coding challenge.
-'''
+"""Module calculates the fewest number of operations needed to
+result in exactly `n` H characters in the file.
+"""
 
 
 def minOperations(n):
-    '''Computes the fewest number of operations needed to result
-    in exactly n H characters.
-    '''
-    if not isinstance(n, int):
+    """Calculate the minimum number of operations to reach `n` H characters.
+    Operations allowed:
+    - Copy All
+    - Paste
+    """
+    if not isinstance(n, int) or n <= 0:
         return 0
-    ops_count = 0
-    clipboard = 0
-    done = 1
-    # print('H', end='')
-    while done < n:
+
+    # Initialize variables
+    ops_count = 0  # Total operations
+    clipboard = 0  # Number of characters in the clipboard
+    current = 1    # Initial count of H characters in the file
+
+    # While the desired number of H characters hasn't been reached
+    while current < n:
+        # If the clipboard is empty, copy all and paste
         if clipboard == 0:
-            # init (the first copy all and paste)
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif n - done > 0 and (n - done) % done == 0:
-            # copy all and paste
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif clipboard > 0:
-            # paste
-            done += clipboard
-            ops_count += 1
-            # print('-(01)->{}'.format('H' * done), end='')
-    # print('')
+            clipboard = current
+            current += clipboard
+            ops_count += 2  # Two operations: Copy All, Paste
+
+        # If the remaining amount to reach `n` is divisible by `current`
+        elif (n - current) % current == 0:
+            clipboard = current
+            current += clipboard
+            ops_count += 2  # Two operations: Copy All, Paste
+
+        # Otherwise, just paste the clipboard's content
+        else:
+            current += clipboard
+            ops_count += 1  # One operation: Paste
+
     return ops_count
